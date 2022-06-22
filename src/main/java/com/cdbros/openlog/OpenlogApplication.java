b374997da2eb7ec9452a0f9fb0fd512d25891c7c
Danilo Cadeddu <danilovillacidro@gmail.com>
2022-06-22 19:55:16 +0200

    feat: first commit spring initializr

---
+++ src/main/java/com/cdbros/openlog/OpenlogApplication.java
@@ -0,0 +1,13 @@
+package com.cdbros.openlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cdbros/openlog/OpenlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdbros.openlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
