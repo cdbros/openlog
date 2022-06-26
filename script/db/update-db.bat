c7edc66f751e2bf510aba289d54226547ba84218
Danilo Cadeddu <danilovillacidro@gmail.com>
2022-06-26 09:55:32 +0200

    chore: liquibase

---
+++ script/db/update-db.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set MYSQL_HOST=127.0.0.1
+set MYSQL_PORT=3306
+set MYSQL_USER=root
+set MYSQL_PASSWORD=root
+set MYSQL_SCHEMA=openlog
+
+java -jar ../../liquibase/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="%MYSQL_USER%" --password="%MYSQL_PASSWORD%" --url="jdbc:mysql://%MYSQL_HOST%:%MYSQL_PORT%/%MYSQL_SCHEMA%?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
---
+++ script/db/update-db.sh
@@ -0,0 +1,7 @@
+MYSQL_HOST=127.0.0.1
+MYSQL_PORT=3306
+MYSQL_USER=root
+MYSQL_PASSWORD=root
+MYSQL_SCHEMA=openlog
+
+java -jar ../../liquibase/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="$MYSQL_USER" --password="$MYSQL_PASSWORD" --url="jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_SCHEMA?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
