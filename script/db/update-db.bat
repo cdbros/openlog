722b0cdb4823ea7c6ae032b9b47d50b96e507e82
Danilo Cadeddu <danilovillacidro@gmail.com>
2022-06-29 10:46:16 +0200

    refactor: liquibase folder

--- script/db/update-db.bat
+++ script/db/update-db.bat
@@ -6,4 +6,4 @@ set MYSQL_USER=root
 set MYSQL_PASSWORD=root
 set MYSQL_SCHEMA=openlog
 
-java -jar ../../liquibase/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="%MYSQL_USER%" --password="%MYSQL_PASSWORD%" --url="jdbc:mysql://%MYSQL_HOST%:%MYSQL_PORT%/%MYSQL_SCHEMA%?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
+java -jar ../../liquibase/lib/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="%MYSQL_USER%" --password="%MYSQL_PASSWORD%" --url="jdbc:mysql://%MYSQL_HOST%:%MYSQL_PORT%/%MYSQL_SCHEMA%?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/lib/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
--- script/db/update-db.sh
+++ script/db/update-db.sh
@@ -4,4 +4,4 @@ MYSQL_USER=root
 MYSQL_PASSWORD=root
 MYSQL_SCHEMA=openlog
 
-java -jar ../../liquibase/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="$MYSQL_USER" --password="$MYSQL_PASSWORD" --url="jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_SCHEMA?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
+java -jar ../../liquibase/lib/liquibase-core-4.3.1.jar --changeLogFile="../../liquibase/changeLog-master.xml" --username="$MYSQL_USER" --password="$MYSQL_PASSWORD" --url="jdbc:mysql://$MYSQL_HOST:$MYSQL_PORT/$MYSQL_SCHEMA?useSSL=false&allowPublicKeyRetrieval=true&serverTimezone=UTC" --classpath="../../liquibase/lib/mysql-connector-java-8.0.19.jar" --driver="com.mysql.cj.jdbc.Driver" --logLevel="info" update
